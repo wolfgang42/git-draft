80f608dfd7a1bcdfe41c787164bbe94c9f465a70
Wolfgang Faust <wolfgang42@git>
2021-09-28 21:12:09 -0700

    Support --message flag for commit

--- src/commit_command.sh
+++ src/commit_command.sh
@@ -1,4 +1,9 @@
 # TODO support amend drafts
 git add -A
+
+git_commit_args=()
+if [[ -v args[--message] ]]; then
+	git_commit_args+=(--message "${args[--message]}")
+fi
 # TODO support $(active_draft_editmsg_file)
-git commit
+git commit "${git_commit_args[@]}"
