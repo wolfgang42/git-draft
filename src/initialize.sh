ba61e1a58da4a890d6714514f0be66d4b0478955
Wolfgang Faust <wolfgang42@git>
2021-10-08 20:38:15 -0700

    Move git_draft() self-call function to initialize

--- src/initialize.sh
+++ src/initialize.sh
@@ -4,3 +4,8 @@
 # CONFIG_FILE=settings.ini
 
 # TODO set -eu -o pipefail # Exit on error, no unset variables, failure in pipeline is error
+
+# Use this function internally to call ourself, so it'll work no matter how the script is installed.
+git_draft() {
+	"$0" "$@"
+}
