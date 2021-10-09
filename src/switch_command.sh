947adf20c3d10c84367e5a36d3b338ea9a18e47d
Wolfgang Faust <wolfgang42@git>
2021-10-08 23:37:54 -0700

    switch: support switching from dirty active draft

--- src/switch_command.sh
+++ src/switch_command.sh
@@ -1,6 +1,5 @@
-if [[ "$(git_draft active-is-empty)" != "y" ]]; then
-	echo 'Switching with a non-empty active draft is not yet implemented' >&2
-	exit 2
+if ! active_is_empty; then
+	git_draft stash-active
 fi
 
 git_draft activate-stashed "${args[draft-name]}"
---
+++ tests/command_switch/basic_switch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source "tests/test_lib"
+
+test_init
+test_init_git_fixture
+
+echo 'foo' > a.txt
+echo 'hi' > example.txt
+
+GIT_DRAFT_TEST_RANDOM_6CHAR=abcdef git-draft create
+
+GIT_DRAFT_TEST_RANDOM_6CHAR=ghijkl git-draft switch main-abcdef
+
+git status
+git-draft list
+git-draft show main-ghijkl
