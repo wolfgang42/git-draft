22f03c71ae417e77e564d2c40504eeb31ad538c1
Wolfgang Faust <wolfgang42@git>
2021-10-08 19:18:53 -0700

    Implement drop command

---
+++ src/drop_command.sh
@@ -0,0 +1,12 @@
+if draft_is_active "${args[draft-name]}"; then
+	echo "Cannot drop an active draft." 2>&1
+	exit 1
+fi
+
+draft_ref="refs/drafts/${args[draft-name]}"
+
+commit="$(git rev-parse "$draft_ref")"
+
+echo "Dropped $draft_ref ($commit)"
+
+git update-ref -d "$draft_ref" "$commit"
