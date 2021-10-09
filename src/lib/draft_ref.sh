9ea70999ba220f5564770059fb85543252248b43
Wolfgang Faust <wolfgang42@git>
2021-10-08 21:11:54 -0700

    Fix shellcheck complaints

--- src/lib/draft_ref.sh
+++ src/lib/draft_ref.sh
@@ -1,8 +1,10 @@
+# shellcheck disable=SC2034
+# ^ "appears unused" - it's not used in this file, but is elsewhere in the script
 declare -r active_draft_ref=refs/git-draft/active
 
 ref_from_name() {
 	if [[ "$1" == refs/drafts/* ]]; then
-		echo $1
+		echo "$1"
 	else
 		echo "refs/drafts/$1"
 	fi
