2188d8f7ce959fbf49f251a5be420923577e8d44
Wolfgang Faust <wolfgang42@git>
2021-10-08 20:11:13 -0700

    Implement create --delete-from-worktree

--- src/create_command.sh
+++ src/create_command.sh
@@ -13,3 +13,7 @@ draft_name="$(generate_draft_name)"
 git update-ref "refs/drafts/$draft_name" "$draft_commit" ''
 
 echo "created new draft $draft_name"
+
+if [[ -v args[--delete-from-worktree] ]]; then
+	git_draft apply "$draft_name" --reverse
+fi
