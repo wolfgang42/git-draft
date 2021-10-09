549af4a8f00b814ba2021aa507ecd6bbdf123956
Wolfgang Faust <wolfgang42@git>
2021-10-08 17:45:26 -0700

    Implement rudimentary list command

---
+++ src/list_command.sh
@@ -0,0 +1,12 @@
+active_draft="$(git symbolic-ref refs/git-draft/active | sed 's%refs/drafts/%%')"
+
+git show-ref | cut -d' ' -f2- | grep -E 'refs/drafts/' | sed 's%refs/drafts/%%' | while read draft ; do
+	if [[ "$draft" == "$active_draft" ]]; then
+		echo -n '* '
+	else
+		echo -n '  '
+	fi
+	echo -n "$draft "
+	# TODO describe
+	git log --oneline --format=%B -n 1 refs/drafts/main-cgqvlx | head -n1 # TODO get message properly
+done
