af8b08ae78e2e71d10d0bd47571a54d271c0cdd9
Wolfgang Faust <wolfgang42@git>
2021-11-02 17:51:10 -0700

    list: embolden draft name

--- src/list_command.sh
+++ src/list_command.sh
@@ -4,16 +4,16 @@
 if ! active_is_empty; then
 	echo -n '* '
 	if active_draft_has_name; then
-		echo -n "$(active_draft_name) "
+		echo -n "$(bold "$(active_draft_name)") "
 	else
-		echo -n "active "
+		echo -n "$(bold active) "
 	fi
 	echo -n "$(git_draft describe active) "
 	git_draft get-commit-message active | head -n1 # TODO get message properly
 fi
 
 git show-ref | cut -d' ' -f2- | (grep -E 'refs/drafts/'||true) | sed 's%refs/drafts/%%' | while read draft ; do
-	echo -n "  $draft "
+	echo -n "  $(bold "$draft") "
 	echo -n "$(git_draft describe "$draft") "
 	git log --oneline --format=%B -n 1 "refs/drafts/$draft" | head -n1 # TODO get message properly
 done
