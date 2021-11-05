5f64a3af500b3cb6f546607ce07c1f6302fd20dd
Wolfgang Faust <wolfgang42@git>
2021-11-04 23:05:44 -0700

    Add brackets around draft description in list output to improve clarity

--- src/list_command.sh
+++ src/list_command.sh
@@ -8,12 +8,12 @@ if ! active_is_empty; then
 	else
 		echo -n "$(bold active) "
 	fi
-	echo -n "$(git_draft describe active) "
+	echo -n "[$(git_draft describe active)] "
 	git_draft get-commit-message --for-draft=active | head -n1 # TODO get message properly
 fi
 
 git show-ref | cut -d' ' -f2- | (grep -E 'refs/drafts/'||true) | sed 's%refs/drafts/%%' | while read draft ; do
 	echo -n "  $(bold "$draft") "
-	echo -n "$(git_draft describe "$draft") "
+	echo -n "[$(git_draft describe "$draft")] "
 	git log --oneline --format=%B -n 1 "refs/drafts/$draft" | head -n1 # TODO get message properly
 done
