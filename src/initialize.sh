532a272389915bf990430a6aa4571140f6f0e70c
Wolfgang Faust <wolfgang42@git>
2021-11-21 19:21:43 -0800

    Use set -u
    Now that Bashly supports doing this

--- src/initialize.sh
+++ src/initialize.sh
@@ -9,8 +9,7 @@
 ## setting environment vairables:
 ## CONFIG_FILE=settings.ini
 
-# TODO set -eu -o pipefail # Exit on error, no unset variables, failure in pipeline is error
-set -e -o pipefail
+set -eu -o pipefail # Exit on error, no unset variables, failure in pipeline is error
 
 # Use this function internally to call ourself, so it'll work no matter how the script is installed.
 git_draft() {
